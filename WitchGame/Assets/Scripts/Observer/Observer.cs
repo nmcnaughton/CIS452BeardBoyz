1bd17eb2916132ea3c4f28ce71e1805f8802bbbe
Nathan McNaughton <namcn@live.com>
2020-04-10 20:27:44 -0500

    Observer Dev and Singleton changes

---
+++ WitchGame/Assets/Scripts/Observer/Observer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Observer<T> : MonoBehaviour where T : Component
+{
+    void OnNotify()
+    {
+
+    }
+}
