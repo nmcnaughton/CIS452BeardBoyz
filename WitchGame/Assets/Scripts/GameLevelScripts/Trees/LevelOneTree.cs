d2a52945320c9f53ffe820ba5a4a3e3609a365b0
sangar3 <55110746+sangar3@users.noreply.github.com>
2020-04-27 16:26:58 -0500

    Cleaned up the project scenes and created Level1
    added the first quest to inform the player to how to move right and to reach the tree to progress in the level

---
+++ WitchGame/Assets/Scripts/GameLevelScripts/Trees/LevelOneTree.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelOneTree : MonoBehaviour
+{
+    public GameObject QuestOne;
+    public GameObject Questtwo;
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Quest 1 Complete: Player touched tree");
+            QuestOne.SetActive(false);
+            Questtwo.SetActive(true);
+
+
+        }
+    }
+}
