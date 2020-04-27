761b5ea220e162ae972666b5d02d83cfb533432c
sangar3 <55110746+sangar3@users.noreply.github.com>
2020-04-27 18:54:09 -0500

    added Jumping Quest
    When the player jumps on the platform it progresses to the next quest

--- WitchGame/Assets/Scripts/GameLevelScripts/TutorialScripts/LevelOneTree.cs
+++ WitchGame/Assets/Scripts/GameLevelScripts/TutorialScripts/LevelOneTree.cs
@@ -6,13 +6,16 @@ public class LevelOneTree : MonoBehaviour
 {
     public GameObject QuestOne;
     public GameObject Questtwo;
+    public bool Quest1Completed;
+
     void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.gameObject.CompareTag("Player"))
+        if (col.gameObject.CompareTag("Player") && !Quest1Completed)
         {
             Debug.Log("Quest 1 Complete: Player touched tree");
             QuestOne.SetActive(false);
             Questtwo.SetActive(true);
+            Quest1Completed = true;
 
 
         }
---
+++ WitchGame/Assets/Scripts/GameLevelScripts/TutorialScripts/LevelTwoJumping.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTwoJumping : MonoBehaviour
+{
+    public GameObject QuestTwo;
+    public GameObject QuestThree;
+    public bool Quest2Completed;
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player") && !Quest2Completed)
+        {
+            Debug.Log("Quest 2 Complete: Player jumped to the platform");
+            QuestTwo.SetActive(false);
+            QuestThree.SetActive(true);
+            Quest2Completed = true;
+
+
+        }
+    }
+}
+
