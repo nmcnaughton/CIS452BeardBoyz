379992811d7791ebe798f8c3262db8ebeffcd821
sangar3 <55110746+sangar3@users.noreply.github.com>
2020-04-28 22:21:30 -0500

    added level design for level 1

---
+++ WitchGame/Assets/Scripts/GameLevelScripts/TutorialScripts/UmbrellaQuest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UmbrellaQuest : MonoBehaviour
+{
+    public GameObject LastQuest;
+    public GameObject TutorialUmbrellaQuest;
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name.Equals("Player"))
+        {
+            Debug.Log("Tell Player about Umbrella");
+            LastQuest.SetActive(false);
+            TutorialUmbrellaQuest.SetActive(true);
+
+
+        }
+    }
+}
