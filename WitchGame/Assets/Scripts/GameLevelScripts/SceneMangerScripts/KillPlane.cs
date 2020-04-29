79b7e5b6df375a6945d3ca864d010b6884ccc978
sangar3 <55110746+sangar3@users.noreply.github.com>
2020-04-28 19:11:54 -0500

    added a kill plane
    it restarts thee level when the player falls off the map

---
+++ WitchGame/Assets/Scripts/GameLevelScripts/SceneMangerScripts/KillPlane.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class KillPlane : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene("level1");
+            Debug.Log("Player fell off Map");
+        }
+    }
+}
