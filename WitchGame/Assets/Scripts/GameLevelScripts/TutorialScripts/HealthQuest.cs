c677502f027700b2d15df28145e2c10511b3f511
sangar3 <55110746+sangar3@users.noreply.github.com>
2020-05-04 14:43:46 -0500

    added health Quest
    a Quest to inform the player about the healthbar

---
+++ WitchGame/Assets/Scripts/GameLevelScripts/TutorialScripts/HealthQuest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthQuest : MonoBehaviour
+{
+    public GameObject Quest5;
+    public GameObject HealthbarQuest;
+    public bool HealthquestComplete;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player") && !HealthquestComplete)
+        {
+            Debug.Log("Quest 2 Complete: Player jumped to the platform");
+            Quest5.SetActive(false);
+            HealthbarQuest.SetActive(true);
+            HealthquestComplete = true;
+
+
+        }
+    }
+
+}
